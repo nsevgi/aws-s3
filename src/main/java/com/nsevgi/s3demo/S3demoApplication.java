39a735df94960c24f9cc650ed437b172005af246
nsevgi <sevgi.nagihan@gmail.com>
2021-05-02 14:18:08 +0300

    first commit

---
+++ src/main/java/com/nsevgi/s3demo/S3demoApplication.java
@@ -0,0 +1,13 @@
+package com.nsevgi.s3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsevgi/s3demo/S3demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsevgi.s3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
